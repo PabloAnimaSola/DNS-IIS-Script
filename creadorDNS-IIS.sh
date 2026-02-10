9f6990fc42aafec080ba2130f0d5b61196bda42a
PabloAnimaSola <118060579+PabloAnimaSola@users.noreply.github.com>
2026-02-10 11:28:10 +0000

    No va hostname, más cosas añadidas

--- creadorDNS-IIS.sh
+++ creadorDNS-IIS.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+## Variables ##
+
+#Nombre de usuario tomado del sistema, ¡ASEGURARSE DE QUE ES SERVERUBUNTUXX!
+user= hostname
+##############
+clear
+echo "Bienvenido al script de creación de páginas de IIS y vinculación de DNS"
+echo "Antes de continuar, asegúrate de tener instalado apache2, bind9, y ufw (sudo apt install apache2 bind9 ufw)"
+echo "Eres el usuario " $user
